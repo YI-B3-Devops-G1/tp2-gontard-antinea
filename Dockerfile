bf73b4271f48157e7fe463213df08dbf0571f4a7
unknown <adntouch@gmail.com>
2020-01-28 15:45:27 +0100

    dockerignore & gitignore, test and node packages

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,8 @@
 FROM node:13.3-alpine
 
-WORKDIR /home/node-app
-RUN chown -R node:node /home/node-app
-USER node
-
+# Copy app and setup node modules
 COPY . .
 RUN npm install
 
 EXPOSE 8000
-CMD ["npm", "start"]
+CMD ["npm", "run","start"]
--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 'use-strict'
 
-// Create instances
 const express = require('express');
 const app = express();
 const PORT = 8000;
@@ -8,7 +7,7 @@ const { Client } = require('pg')
 const redis = require("redis")
 
 
-// Setup instances
+//Set DB instances
 const pgClient = new Client({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -16,14 +15,16 @@ const pgClient = new Client({
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 })
-
 const redisClient = redis.createClient({ host: process.env.REDIS_HOST });
 
+
+//Get Api Home Message
 app.get('/', (req, res) => {
     res.json({message: 'Hello World'})
 });
 
-app.get('/status', (req, res) => {
+//Get DB status
+app.get('/status', async (req, res) => {
     const query = 'SELECT date_trunc(\'second\', current_timestamp - pg_postmaster_start_time()) as uptime;';
     const result = await pgClient.query(query);
     const uptime = result.rows[0].uptime;
@@ -44,6 +45,7 @@ app.get('/status', (req, res) => {
     })
 });
 
+// Start express server 
 app.listen(PORT,(error) =>{
     if(error){throw error; }
 
